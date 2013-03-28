c74512c119d2c1a265ad1b809c42f2922f66e75a
Jinpei Li <jinpeili@yp.com>
2013-03-27 17:15:37 -0700

    Modify a file and add a new class of table cell.

---
+++ TestGitProject/AYTestCell.h
@@ -0,0 +1,13 @@
+//
+//  AYTestCell.h
+//  TestGitProject
+//
+//  Created by Jinpei Li on 3/27/13.
+//  Copyright (c) 2013 Jinpei Li. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AYTestCell : UITableViewCell
+
+@end
---
+++ TestGitProject/AYTestCell.m
@@ -0,0 +1,29 @@
+//
+//  AYTestCell.m
+//  TestGitProject
+//
+//  Created by Jinpei Li on 3/27/13.
+//  Copyright (c) 2013 Jinpei Li. All rights reserved.
+//
+
+#import "AYTestCell.h"
+
+@implementation AYTestCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
